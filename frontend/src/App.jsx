429f3812ddae7f660473f69b7868de3c58de5206
Tanmayraut16 <Tanmayraut16@outlook.com>
2025-01-15 00:11:46 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import SignUp from './Pages/SignUp.jsx'
+import Likes from './Pages/Likes.jsx'
+import Explore from './Pages/Explore.jsx'
+import Sidebar from './components/Sidebar.jsx'
+
+const App = () => {
+  return (
+    <div className='flex text-white'>
+      <Sidebar />
+      <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/explore' element={<Explore/>} />
+          <Route path='/likes' element={<Likes/>} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
