d232380bea72d3adde3357bfe94a20f1e62d250e
Tanmayraut16 <Tanmayraut16@outlook.com>
2025-01-23 23:22:08 +0530

    optimized for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,43 @@
-import express from 'express';
-import dotenv from "dotenv"
-import cors from "cors"
-import passport from 'passport';
-import session from 'express-session';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+import path from "path";
 
-import "./passport/github.auth.js"
+import "./passport/github.auth.js";
 
-import userRoutes from "./routes/user.route.js"
-import authRoutes from "./routes/auth.route.js"
-import exploreRoutes from "./routes/explore.route.js"
-import connectMongoDB from './db/connectMongoDB.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import exploreRoutes from "./routes/explore.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
-
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes)
-app.use("/api/exploreRepos", exploreRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/exploreRepos", exploreRoutes);
 
-app.listen(5000, () => {
-    console.log("Server started at ");
-    connectMongoDB();
-})
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+  connectMongoDB();
+});
